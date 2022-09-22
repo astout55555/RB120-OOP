8536b0c14e6443da19827e9dc622c83a1e67159b
astout55555 <alex.the.stout5@gmail.com>
2022-09-22 15:15:29 -0700

    Ready to branch off for new design, using classes for the various moves.

--- lesson_2/rps.rb
+++ lesson_2/rps.rb
@@ -1,10 +1,11 @@
 ## Classes/methods based on organized nouns/verbs
 
 class Player
-  attr_accessor :move, :name
+  attr_accessor :move, :name, :score
 
   def initialize
     set_name
+    @score = 0
   end
 end
 
@@ -23,8 +24,8 @@ class Human < Player
   def choose
     choice = nil
     loop do
-      puts "Please choose rock, paper, or scissors:"
-      choice = gets.chomp
+      puts "Please choose rock, paper, scissors, lizard, or Spock:"
+      choice = gets.chomp.downcase
       break if Move::VALUES.include?(choice)
       puts 'Sorry, invalid choice.'
     end
@@ -43,7 +44,7 @@ class Computer < Player
 end
 
 class Move
-  VALUES = ['rock', 'paper', 'scissors']
+  VALUES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
   def initialize(value)
     @value = value
@@ -61,16 +62,38 @@ class Move
     @value == 'paper'
   end
 
+  def lizard?
+    @value == 'lizard'
+  end
+
+  def spock?
+    @value == 'spock'
+  end
+
   def >(other_move)
-    (rock? && other_move.scissors?)    ||
-      (paper? && other_move.rock?)     ||
-      (scissors? && other_move.paper?)
+    (rock? && other_move.scissors?)   ||
+    (rock? && other_move.lizard?)     ||
+    (paper? && other_move.rock?)      ||
+    (paper? && other_move.spock?)     ||
+    (scissors? && other_move.paper?)  ||
+    (scissors? && other_move.lizard?) ||
+    (lizard? && other_move.paper?)    ||
+    (lizard? && other_move.spock?)    ||
+    (spock? && other_move.scissors?)  ||
+    (spock? && other_move.rock?)
   end
 
   def <(other_move)
-    (rock? && other_move.paper?)        ||
-      (paper? && other_move.scissors?)  ||
-      (scissors? && other_move.rock?)
+    (rock? && other_move.paper?)      ||
+    (rock? && other_move.spock?)      ||
+    (paper? && other_move.scissors?)  ||
+    (paper? && other_move.lizard?)    ||
+    (scissors? && other_move.rock?)   ||
+    (scissors? && other_move.spock?)  ||
+    (lizard? && other_move.scissors?) ||
+    (lizard? && other_move.rock?)     ||
+    (spock? && other_move.paper?)     ||
+    (spock? && other_move.lizard?)
   end
 
   def to_s
@@ -101,14 +124,15 @@ class RPSGame
   end
 
   def display_welcome_message
-    puts 'Welcome to Rock, Paper, Scissors!'
+    puts "Welcome to Rock, Paper, Scissors, Lizard, Spock, #{human.name}!"
   end
 
   def display_goodbye_message
-    puts 'Thanks for playing Rock, Paper, Scissors. Goodbye!'
+    puts 'Thanks for playing Rock, Paper, Scissors, Lizard, Spock. Goodbye!'
   end
 
   def display_moves
+    puts '------------'
     puts "#{human.name} chose: #{human.move}"
     puts "#{computer.name} chose: #{computer.move}"
   end
@@ -116,10 +140,27 @@ class RPSGame
   def display_winner
     if human.move > computer.move
       puts "#{human.name} won!"
+      human.score += 1
     elsif human.move < computer.move
       puts "#{computer.name} won!"
+      computer.score += 1
     else
       puts "It's a tie!"
+    end
+  end
+
+  def match_over?
+    ((human.score >= 10) || (computer.score >= 10))
+  end
+
+  def display_score
+    puts "You have #{human.score} points."
+    puts "#{computer.name} has #{computer.score} points."
+
+    if human.score >= 10
+      puts "#{human.name} triumphs over #{computer.name}!"
+    elsif computer.score >= 10
+      puts "Oh no, #{human.name} has been vanquished by #{computer.name}!"
     end
   end
 
@@ -139,10 +180,14 @@ class RPSGame
   def play
     display_welcome_message
     loop do
-      human.choose
-      computer.choose
-      display_moves
-      display_winner
+      loop do
+        human.choose
+        computer.choose
+        display_moves
+        display_winner
+        display_score
+        break if match_over?
+      end
       break unless play_again?
     end
     display_goodbye_message
