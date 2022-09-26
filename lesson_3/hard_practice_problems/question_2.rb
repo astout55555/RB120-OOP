0f4df7087ea777e7aeb48da113ac157c92901d74
astout55555 <alex.the.stout5@gmail.com>
2022-09-26 14:08:43 -0700

    Done with lesson 4 practice problems.

---
+++ lesson_3/hard_practice_problems/question_2.rb
@@ -0,0 +1,77 @@
+# building off the previous LS solution using a module:
+
+module Moveable
+  attr_accessor :speed, :heading
+  attr_writer :fuel_capacity, :fuel_efficiency
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class WheeledVehicle
+  include Moveable
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+end
+
+class Catamaran < Boat
+  attr_reader :propeller_count, :hull_count
+
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    super(km_traveled_per_liter, liters_of_fuel_capacity)
+    @num_propellers = num_propellers
+    @num_hulls = num_hulls
+    # ... other code to track catamaran-specific data omitted ...
+  end
+end
+
+## we must also track a basic motorboat:
+
+class Boat
+  include Moveable
+
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+end
+
+class Motorboat < Boat; end
+
+## LS solution still gives Motorboat instance vars for hulls and propellers, just sets them to 1
+
+class Seacraft
+  include Moveable
+
+  attr_reader :hull_count, :propeller_count
+
+  def initialize(num_propellers, num_hulls, fuel_efficiency, fuel_capacity)
+    @propeller_count = num_propellers
+    @hull_count = num_hulls
+    self.fuel_efficiency = fuel_efficiency
+    self.fuel_capacity = fuel_capacity
+  end
+end
+
+class Motorboat < Seacraft
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    # set up 1 hull and 1 propeller
+    super(1, 1, km_traveled_per_liter, liters_of_fuel_capacity)
+  end
+end
+
+class Catamaran < Seacraft
+end
