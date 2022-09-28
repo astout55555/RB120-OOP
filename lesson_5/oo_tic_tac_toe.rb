7c3beb0106860d246614c760bf3fb3f8c02ea042
astout55555 <alex.the.stout5@gmail.com>
2022-09-28 11:55:55 -0700

    Added all additional features for TTT game. Significant rubocop complaints still need to be addressed, code refactored.

--- lesson_5/oo_tic_tac_toe.rb
+++ lesson_5/oo_tic_tac_toe.rb
@@ -1,5 +1,85 @@
 require 'pry-byebug'
 
+# to store display methods which would otherwise clog up the TTTGame class
+module Displayable
+  def display_end_of_match_message
+    if human.score >= 5
+      puts "(to the crowd) #{human.name} has achieved victory!! *wild applause*"
+      puts "Hey, nice job out there. You made me proud."
+    else
+      puts "#{human.name} has been defeated! #{computer.name} is the champion!"
+      puts "Better luck next time!"
+    end
+  end
+
+  def display_scores
+    puts "#{human.name} has: #{human.score} points."
+    puts "#{computer.name} has: #{computer.score} points."
+  end
+
+  def display_welcome_message
+    puts "Welcome to Tic Tac Toe!"
+    puts ""
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Tic Tac Toe! Goodbye!"
+  end
+
+  def display_play_again_message
+    puts "Okay, let's play again! I'll set you up with a new computer opponent."
+    puts ""
+  end
+
+  def display_result
+    clear_screen_and_display_board
+
+    if human_won?
+      puts "You won!"
+    elsif computer_won?
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
+    end
+  end
+
+  def clear_screen_and_display_board
+    clear
+    display_board
+  end
+
+  def display_board
+    puts "You're a #{human.marker}. #{computer.name} is a #{computer.marker}."
+    puts ""
+    board.draw
+    puts ""
+  end
+
+  def joinor(array, separator_string = ', ', final_separator = 'or')
+    if array.size > 2
+      final_element = array.pop
+      mostly_joined = array.join(separator_string)
+      "#{mostly_joined}#{separator_string}#{final_separator} #{final_element}"
+    else
+      array.join(" #{final_separator} ")
+    end
+  end
+
+  def list_valid_moves
+    valid_moves = board.unmarked_keys
+    joinor(valid_moves)
+  end
+
+  def clear
+    system "clear"
+  end
+
+  def pause_prompt
+    puts "Hit enter to continue."
+    gets
+  end
+end
+
 class Board
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                   [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
@@ -101,15 +181,47 @@ class Square
 end
 
 class Player
-  attr_accessor :marker, :score
+  attr_accessor :score
+  attr_reader :name, :marker
 
-  def initialize(marker)
-    @marker = marker
+  def initialize
     @score = 0
+    set_name
+  end
+end
+
+class Human < Player
+  def initialize
+    super
+    choose_marker
+  end
+
+  def set_name
+    answer = nil
+    loop do
+      puts "What's your name, stranger?"
+      answer = gets.chomp.strip
+      break if answer != ''
+      puts "Sorry, I didn't catch that. You have a name, right?"
+    end
+    @name = answer
+  end
+
+  def choose_marker
+    character = nil
+    loop do
+      puts "What would you like to use as your marker? Enter any character."
+      character = gets.chomp.strip
+      break if character.length == 1
+      puts "Sorry, choose a single character (and not an empty space)."
+    end
+    @marker = character
   end
 end
 
 class Computer < Player
+  include Displayable
+
   DIFFICULTY_RATINGS = {
     'easy' => 1,
     'medium' => 2,
@@ -117,118 +229,83 @@ class Computer < Player
     'demonic' => 4
   }
 
+  COMPUTER_NAMES = [
+    ['R1', 'R2', 'R3', 'R4'],
+    ['D4', 'D6', 'D8', 'D12', 'D20']
+  ]
+
   attr_reader :difficulty
 
-  def initialize(marker, difficulty: 'hard')
-    super(marker)
-    @difficulty = DIFFICULTY_RATINGS[difficulty]
+  attr_writer :marker
+
+  def initialize
+    super
+    @marker = 'O'
+    give_difficulty_choice
   end
 
-  def difficulty=(new_difficulty)
-    @difficulty = DIFFICULTY_RATINGS[new_difficulty]
+  def set_name
+    @name = COMPUTER_NAMES[0].sample + COMPUTER_NAMES[1].sample
+  end
+
+  def difficulty=(difficulty_label)
+    @difficulty = DIFFICULTY_RATINGS[difficulty_label]
+  end
+
+  def give_difficulty_choice
+    puts "Do you want to pick the difficulty? (y/n)"
+    answer = gets.chomp.strip.downcase
+    if answer == 'y' || answer == 'yes'
+      prompt_for_difficulty
+    else
+      puts "You're not all that excited to pick, huh? Don't worry, I'll do it."
+      self.difficulty = DIFFICULTY_RATINGS.keys.sample
+    end
+  end
+
+  def prompt_for_difficulty
+    answer = nil
+    loop do
+      puts "How tough do you want your opponent to be?"
+      puts "Choose: #{joinor(DIFFICULTY_RATINGS.keys)}"
+      answer = gets.chomp.downcase
+      break if DIFFICULTY_RATINGS.keys.include?(answer)
+      puts "Sorry, please choose one of the listed difficulty options."
+    end
+
+    self.difficulty = answer
+  end
+
+  def choose_first_to_move
+    return 'computer' if difficulty > 2
+    return ['h', 'c'].sample if difficulty == 2
+    'human'
   end
 end
 
-module Displayable
-  def display_end_of_match_message
-    if human.score >= 5
-      puts "You won the whole match! Nice job!"
-    else
-      puts "Oh shoot, you lost the match! Better luck next time!"
-    end
-  end
-
-  def display_scores
-    puts "You have: #{human.score} points."
-    puts "Computer has: #{computer.score} points."
-  end
-
-  def display_welcome_message
-    puts "Welcome to Tic Tac Toe!"
-    puts ""
-  end
-
-  def display_goodbye_message
-    puts "Thanks for playing Tic Tac Toe! Goodbye!"
-  end
-
-  def display_play_again_message
-    puts "Let's play again!"
-    puts ""
-  end
-
-  def display_result
-    clear_screen_and_display_board
-
-    if human_won?
-      puts "You won!"
-    elsif computer_won?
-      puts "Computer won!"
-    else
-      puts "It's a tie!"
-    end
-  end
-
-  def clear_screen_and_display_board
-    clear
-    display_board
-  end
-
-  def display_board
-    puts "You're a #{human.marker}. Computer is a #{computer.marker}."
-    puts ""
-    board.draw
-    puts ""
-  end
-
-  def joinor(array, separator_string = ', ', final_separator = 'or')
-    if array.size > 2
-      final_element = array.pop
-      mostly_joined = array.join(separator_string)
-      "#{mostly_joined}#{separator_string}#{final_separator} #{final_element}"
-    else
-      array.join(" #{final_separator} ")
-    end
-  end
-
-  def list_valid_moves
-    valid_moves = board.unmarked_keys
-    joinor(valid_moves)
-  end
-
-  def clear
-    system "clear"
-  end
-
-  def pause_prompt
-    puts "Hit enter to continue."
-    gets
-  end
-end
+## Orchestration Engine
 
 class TTTGame
-  HUMAN_MARKER = "X"
-  COMPUTER_MARKER = "O"
-  FIRST_TO_MOVE = HUMAN_MARKER
-
   attr_reader :board, :human, :computer
 
   def initialize
+    clear
+    display_welcome_message
     @board = Board.new
-    @human = Player.new(HUMAN_MARKER)
-    @computer = Computer.new(COMPUTER_MARKER)
-    @current_marker = FIRST_TO_MOVE
+    @human = Human.new
+    @computer = Computer.new
+    computer.marker = 'X' if %(o O 0).include?(human.marker)
+    determine_first_to_move
+    @current_marker = @first_to_move
   end
 
   def play
+    pause_prompt
     clear
-    display_welcome_message
-    choose_difficulty
     loop do
       main_game
       break unless play_again?
       game_reset
-      display_play_again_message
     end
     display_goodbye_message
   end
@@ -236,6 +313,39 @@ class TTTGame
   private
 
   include Displayable
+
+  def determine_first_to_move
+    if human_picking_first?
+      to_go_first = nil
+      loop do
+        puts "Okay, choose who goes first. Human (h) or computer (c)."
+        to_go_first = gets.chomp.strip.downcase
+        break if %(human h computer c).include?(to_go_first)
+        puts "Umm, what? Please just say 'human' or 'computer'--or 'h' or 'c'."
+      end
+    else
+      puts "I guess we'll let the computer pick who goes first then."
+      to_go_first = computer.choose_first_to_move
+    end
+
+    @first_to_move = if to_go_first == 'h' || to_go_first == 'human'
+                       human.marker
+                     else
+                       computer.marker
+                     end
+  end
+
+  def human_picking_first?
+    answer = nil
+    loop do
+      puts "Do you want to pick who goes first? (y/n)"
+      answer = gets.chomp.strip.downcase
+      break if %w(y yes n no).include?(answer)
+      puts "I didn't understand you. Please enter 'y' or 'n'."
+    end
+
+    answer == 'y' || answer == 'yes'
+  end
 
   def main_game
     loop do
@@ -246,19 +356,6 @@ class TTTGame
     end
 
     display_end_of_match_message
-  end
-
-  def choose_difficulty
-    answer = nil
-    loop do
-      puts "How tough do you want your opponent to be?"
-      puts "Choose: #{joinor(Computer::DIFFICULTY_RATINGS.keys)}"
-      answer = gets.chomp.downcase
-      break if Computer::DIFFICULTY_RATINGS.keys.include?(answer)
-      puts "Sorry, please choose one of the listed difficulty options."
-    end
-
-    computer.difficulty = answer
   end
 
   def resolve_round
@@ -296,7 +393,7 @@ class TTTGame
   end
 
   def human_turn?
-    @current_marker == HUMAN_MARKER
+    @current_marker == human.marker
   end
 
   def human_moves
@@ -348,10 +445,10 @@ class TTTGame
   def current_player_moves
     if human_turn?
       human_moves
-      @current_marker = COMPUTER_MARKER
+      @current_marker = computer.marker
     else
       computer_moves
-      @current_marker = HUMAN_MARKER
+      @current_marker = human.marker
     end
   end
 
@@ -360,26 +457,31 @@ class TTTGame
     loop do
       puts "Would you like to play again? (y/n)"
       answer = gets.chomp.downcase
-      break if %w(y n).include? answer
-      puts "Sorry, must be y or n"
+      break if %w(y n yes no).include? answer
+      puts "Sorry, must be 'y' or 'n'."
     end
 
-    answer == 'y'
+    answer == 'y' || answer == 'yes'
   end
 
   def round_reset
     board.reset
-    @current_marker = FIRST_TO_MOVE
+    @current_marker = @first_to_move
     clear
   end
 
   def game_reset
+    clear
+    display_play_again_message
+    @computer = Computer.new
+    determine_first_to_move
+    pause_prompt
     round_reset
     human.score = 0
-    computer.score = 0
-    choose_difficulty
   end
 end
+
+## Code to execute
 
 game = TTTGame.new
 game.play
